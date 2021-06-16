ad2124f7858ff6fab9fed70ac719be8ce096b2eb
felipe-franca <felipe.paulino57@gmail.com>
2021-06-15 23:22:13 -0300

    Adicionado exit codes para diagnosticos de erro de execução.

--- vacuumdb.sh
+++ vacuumdb.sh
@@ -18,50 +18,17 @@ for db in $@; do
     EXIT_CODE=0
     KILL_LONG_CONNECTIONS="SELECT pg_terminate_backend(procpid) FROM pg_stat_activity WHERE query_start < ( now() - INTERVAL '2 hours'  and datname = '$DB')"
 
-    MAIN_TABLES=(
-        "sincronizacao_enterprise"
-        "ticketmobile"
-        "cartoes"
-        "ociosidade"
-        "ociosidadeporgrupo"
-        "valorcontador"
-        "lot"
-        "movimento_por_horario"
-        "estatistica_por_permanencia"
-        "configuracoes"
-        "detalhetickets"
-        "somas_faturamento_trn"
-        "somas_tipo_trn"
-        "somaspermanencia"
-        "somas_pagamento_trn"
-        "somas_pagamento_trn_parcial"
-        "somas_vendas_trn"
-        "somas_fechafinan_arrecadacao_trn"
-        "estat_permanencia_pos_pagto"
-        "dailies"
-        "somas"
-        "somasparcial"
-        "estatistica_por_permanencia_setor"
-        "valores_parcial"
-        "valores"
-        "receita_por_tarifa_parcial"
-        "receita_por_tarifa_vendas"
-        "estatistica_pagantes_abonados"
-        "receita_por_tarifa"
-        "patio"
-        "recibos"
-        );
+    MAIN_TABLES=( "sincronizacao_enterprise" "ticketmobile" "cartoes" "ociosidade"
+                  "ociosidadeporgrupo" "valorcontador" "lot" "movimento_por_horario"
+                  "estatistica_por_permanencia" "configuracoes" "detalhetickets" "somas_faturamento_trn"
+                  "somas_tipo_trn" "somaspermanencia" "somas_pagamento_trn" "somas_pagamento_trn_parcial"
+                  "somas_vendas_trn" "somas_fechafinan_arrecadacao_trn" "estat_permanencia_pos_pagto" "dailies"
+                  "somas" "somasparcial" "estatistica_por_permanencia_setor" "valores_parcial" "valores"
+                  "receita_por_tarifa_parcial" "receita_por_tarifa_vendas" "estatistica_pagantes_abonados" 
+                  "receita_por_tarifa" "patio" "recibos" );
 
-    TRUNC_TABLES=(
-        "arqueo"
-        "seqs_uruguai_por_terminal"
-        "seqs_uruguai_por_terminal_parcial"
-        "controle_sequenciais_uruguai"
-        "ppmestadoestacao"
-        "sinalcontagem"
-        "etstickets_temp"
-        "imagenslpr"
-        );
+    TRUNC_TABLES=( "arqueo" "seqs_uruguai_por_terminal" "seqs_uruguai_por_terminal_parcial"
+                   "controle_sequenciais_uruguai" "ppmestadoestacao" "sinalcontagem" "etstickets_temp" "imagenslpr" );
 
     QUERY=
 
@@ -75,6 +42,11 @@ for db in $@; do
                     $DOLOG "[INFO] Operation '$1' started the ${ARRAY_ARGS[y]} table - $(date +%T)"
                     $DOLOG "[INFO] $PSQL \"${ARRAY_ARGS[y]};\""
                         $PSQL "${ARRAY_ARGS[y]};"
+                    if [ $? -ne 0 ]; then
+                        $DOLOG "[WARN] Something wrong -> $1 | ${ARRAY_ARGS[y]} "
+                    else
+                        $DOLOG "[INFO] Succesfuly."
+                    fi
                 $DOLOG "[INFO] Finalized '$1' operation on ${ARRAY_ARGS[y]} table - $(date +%T)"
             done
             return 0;
@@ -85,6 +57,11 @@ for db in $@; do
                     $DOLOG "[INFO] Operation '$1' started the ${ARRAY_ARGS[y]} table - $(date +%T)"
                     $DOLOG "[INFO] $PSQL \"$1 ${ARRAY_ARGS[y]};\""
                         $PSQL "$1 ${ARRAY_ARGS[y]};"
+                    if [ $? -ne 0 ]; then
+                        $DOLOG "[WARN] Something wrong -> $1 | ${ARRAY_ARGS[y]} "
+                    else 
+                        $DOLOG "[INFO] Succesfuly."
+                    fi
                 $DOLOG "[INFO] Finalized '$1' operation on ${ARRAY_ARGS[y]} table - $(date +%T)"
             done
             return 0;
@@ -92,7 +69,6 @@ for db in $@; do
     }
 
     $DOLOG "Maintenance will be started on Container id - $CONTAINER - database: $DB"
-
     $DOLOG "[INFO] Cheking long connections . . ."
 
     HAVE_LONG_CON=$($PSQL "SELECT count(procpid) FROM pg_catalog.pg_stat_activity WHERE query_start <= (now() - INTERVAL '2 hours') and datname like '$DB';" --tuples-only | sed 's/ //g')
@@ -105,12 +81,16 @@ for db in $@; do
         $DOLOG "[INFO] Killing longconnections . . ."
             $DOLOG "[DEBUG] QUERY: $PSQL $KILL_LONG_CONNECTIONS "
             $PSQL "$KILL_LONG_CONNECTIONS"
-        $DOLOG "[INFO] Done !"
+        if [ $? -eq 0 ]; then
+            $DOLOG "[INFO] Done !"
+        else
+            $DOLOG "[WARN] Something wrong in execution var KILL_LOG_CONNECTIONS."
+        fi
     else
-        $DOLOG "[INFO] No long connectios found !"
+        $DOLOG "[INFO] No long connectios found ! : $HAVE_LONG_CON"
     fi
 
-    $DOLOG "[INFO] Maintenance tables:: ${MAIN_TABLES[@]}"
+    $DOLOG "[INFO] Maintenance tables: ${MAIN_TABLES[@]}"
     $DOLOG "[INFO] Tables to be truncated: ${TRUNC_TABLES[@]}"
 
     OPERATION=( "QUERY" "TRUNCATE TABLE" "VACUUM FULL ANALYSE VERBOSE" "REINDEX TABLE" )
@@ -127,32 +107,24 @@ for db in $@; do
                 $DOLOG "[INFO] Calls maintenance function."
                 $DOLOG "[INFO] Call: execParams | operation: ${OPERATION[i]} | Quantity: ${#QUERY[@]} | statement: ${QUERY[@]}"
                     execParams "${OPERATION[i]}" "QUERY[@]"
-                    EXIT_CODE=$?
-                $DOLOG "[INFO] Operation ended."
+                $DOLOG "[INFO] Operation ${OPERATION[i]} ended."
             fi
         elif [ "${OPERATION[i]}" = "TRUNCATE TABLE" ]; then
             $DOLOG "[INFO] Calls maintenance function."
             $DOLOG "[INFO] Call: execParams | operation: ${OPERATION[i]} | Quantity: ${#TRUNC_TABLES[@]} | tables: ${TRUNC_TABLES[@]}"
                 execParams "${OPERATION[i]}" "TRUNC_TABLES[@]"
-                EXIT_CODE=$?
-            $DOLOG "[INFO] Operation ended."
+            $DOLOG "[INFO] Operation ${OPERATION[i]} ended."
 
         elif [ "${OPERATION[i]}" = "VACUUM FULL ANALYSE VERBOSE" ] || [ "${OPERATION[i]}" = "REINDEX TABLE" ]; then
             $DOLOG "[INFO] Calls maintenance function."
             $DOLOG "[INFO] Call: execParams | operation: ${OPERATION[i]} | Quantity: ${#MAIN_TABLES[@]} | tables: ${MAIN_TABLES[@]}"
                 execParams "${OPERATION[i]}" "MAIN_TABLES[@]"
-                EXIT_CODE=$?
-            $DOLOG "[INFO] Operation ended."
+            $DOLOG "[INFO] Operation ${OPERATION[i]} ended."
 
         else
             $DOLOG "[WARN] Unknow Operation."
             continue;
         fi
-
-        if [ ! $EXIT_CODE -eq 0 ]; then
-            $DOLOG "[ERROR] Exit code: $EXIT_CODE on operation ${OPERATION[i]}."
-        fi
     done
-
     $DOLOG "[INFO] >>>> Maintenance Finished on database $DB <<<<<"
 done

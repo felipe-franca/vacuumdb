a42ba2993790f44900db9357270e97456ea60eb8
felipe-franca <felipe.paulino57@gmail.com>
2022-04-19 08:52:02 -0300

    correction in tables list

--- cloud/vacummcloud.sh
+++ cloud/vacummcloud.sh
@@ -1,24 +1,18 @@
 #!/usr/bin/env sh
 
 export PGUSER=postgres
-export PGPASSWORD=enterprise@2020
-
-OLD_IFS=$IFS
-
-IFS=' '
+export PGPASSWORD=
 
 HOST_IP="10.27.192.3"
 PSQL="psql -U postgres -h $HOST_IP -c "
 
-#dbs=$($PSQL "SELECT datname FROM pg_database a WHERE datname LIKE '%-enterprise-%'" --tuples-only)
-dbs="parkingplus-enterprise-demonstrar"
+dbs=$($PSQL "SELECT datname FROM pg_database a WHERE datname LIKE '%-enterprise-%'" --tuples-only)
+
 for db in $dbs; do
     echo "Banco -> $db"
     pPSQL="psql -U postgres -h $HOST_IP -d $db -c "
-    declare -a TABLES=$($pPSQL "SELECT DISTINCT tablename  from (select t1.relname as tablename, t2.nspname as schema, t2.nspname || '.' ||t1.relname as schema_table, t1.reltuples as qtd_reg, t1.relowner from pg_catalog.pg_class t1 join pg_catalog.pg_namespace t2 on (t1.relnamespace = t2.oid) where t2.nspname not in ('pg_catalog', 'information_schema', 'pg_toast') and t1.relkind = 'r') as tbs where  pg_total_relation_size(schema_table) >= 606650368" --tuples-only)
 
-    echo "exit code -> $?"
-    echo "tables -> $TABLES"
+    TABLES=($($pPSQL "SELECT DISTINCT tablename  from (select t1.relname as tablename, t2.nspname as schema, t2.nspname || '.' ||t1.relname as schema_table, t1.reltuples as qtd_reg, t1.relowner from pg_catalog.pg_class t1 join pg_catalog.pg_namespace t2 on (t1.relnamespace = t2.oid) where t2.nspname not in ('pg_catalog', 'information_schema', 'pg_toast') and t1.relkind = 'r') as tbs where  pg_total_relation_size(schema_table) >= 606650368" --tuples-only))
 
     execParams() {
         local ARRAY_ARGS=("${!2}")
@@ -46,7 +40,7 @@ for db in $dbs; do
             for ((y = 0; y < ${#ARRAY_ARGS[@]}; y++)); do
                 echo "[INFO] Operation '$1' started the ${ARRAY_ARGS[y]} table - $(date +%T)"
                 echo "[INFO] $pPSQL \"$1 ${ARRAY_ARGS[y]};\""
-                $PSQL "$1 ${ARRAY_ARGS[y]};"
+                $pPSQL "$1 ${ARRAY_ARGS[y]};"
                 if [ $? -ne 0 ]; then
                     echo "[WARN] Something wrong -> $1 | ${ARRAY_ARGS[y]} "
                 else
@@ -81,7 +75,7 @@ for db in $dbs; do
         elif [ "${OPERATION[i]}" = "VACUUM FULL" ] || [ "${OPERATION[i]}" = "REINDEX TABLE" ]; then
             echo "[INFO] Calls maintenance function."
             echo "[INFO] Call: execParams | operation: ${OPERATION[i]} | Quantity: ${#TABLES[@]} | tables: ${TABLES[@]}"
-            execParams "${OPERATION[i]}" "${TABLES[@]}"
+            execParams "${OPERATION[i]}" "TABLES[@]"
             echo "[INFO] Operation ${OPERATION[i]} ended."
 
         else
@@ -91,5 +85,3 @@ for db in $dbs; do
     done
     echo "[INFO] >>>> Maintenance Finished on database $db <<<<<"
 done
-
-IFS=$OLD_IFS
